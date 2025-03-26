da895b683b7c20dd97f479c3778be1a10927a482
rosecodez <rosebeats09@gmail.com>
2025-03-26 17:27:20 -0400

    Add links for skills

--- src/Skills.jsx
+++ src/Skills.jsx
@@ -14,133 +14,170 @@ export default function Skills() {
                     <Row id='skills-icons-row'>
                         <Col>
                             <div className='skill-container'>
-                                <img className='skills-icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-plain.svg" alt='HTML5' />
-                                <p className='skills-text'>
-                                    HTML5
-                                </p>
+                                <a href='https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/HTML5'>
+                                    <img className='skills-icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-plain.svg" alt='HTML5' />
+                                    <p className='skills-text'>
+                                        HTML5
+                                    </p>
+                                </a>
                             </div>
                         </Col>
                         <Col>
                             <div className='skill-container'>
-                                <img className='skills-icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-plain.svg" alt='CSS3' />
-                                <p className='skills-text'>
-                                    CSS3
-                                </p>
+                                <a href='https://developer.mozilla.org/en-US/docs/Web/CSS'>
+                                    <img className='skills-icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-plain.svg" alt='CSS3' />
+                                    <p className='skills-text'>
+                                        CSS3
+                                    </p>
+                                </a>
+                                
                             </div>
                         </Col>
                         <Col>
                             <div className='skill-container'>
-                                <img className='skills-icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg" alt='Bootstrap' />
-                                <p className='skills-text'>
-                                    Bootstrap
-                                </p>
+                                <a href='https://getbootstrap.com/'>
+                                    <img className='skills-icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg" alt='Bootstrap' />
+                                    <p className='skills-text'>
+                                        Bootstrap
+                                    </p>
+                                </a>
+                            </div>   
+                        </Col>
+                        <Col>
+                            <div className='skill-container'>
+                                <a href='https://developer.mozilla.org/en-US/docs/Web/JavaScript'>
+                                    <img className='skills-icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" alt='JavaScript' />
+                                    <p className='skills-text'>
+                                        JavaScript
+                                    </p>
+                                </a>
                             </div>
                         </Col>
                         <Col>
                             <div className='skill-container'>
-                                <img className='skills-icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" alt='JavaScript' />
-                                <p className='skills-text'>
-                                    JavaScript
-                                </p>
-                            </div>
-                        </Col>
-                        <Col>
-                            <div className='skill-container'>
-                                <img className='skills-icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" alt='React' />
-                                <p className='skills-text'>
-                                    React
-                                </p>
-                            </div>
-                        </Col>
-                        
-                        <Col>
-                            <div className='skill-container'>
-                                <img className='skills-icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vitejs/vitejs-original.svg" alt='Vite' />
-                                <p className='skills-text'>
-                                    Vite
-                                </p>
-                            </div>
-                        </Col>
-                        <Col>
-                            <div className='skill-container'>
-                                <img className='skills-icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/webpack/webpack-plain.svg" alt='Webpack' />
-                                <p className='skills-text'>
-                                    Webpack
-                                </p>
-                            </div>
-                        </Col>
-                        <Col>
-                            <div className='skill-container'>
-                                <img className='skills-icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg" alt='Express' />
-                                <p className='skills-text'>
-                                    Express
-                                </p>
+                                <a href='https://reactjs.org/'>
+                                    <img className='skills-icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" alt='React' />
+                                    <p className='skills-text'>
+                                        React
+                                    </p>
+                                </a>
                             </div>
                         </Col>
                         
                         <Col>
                             <div className='skill-container'>
-                                <img className='skills-icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" alt='node.js' />
-                                <p className='skills-text'>
-                                    node.js
-                                </p>
+                                <a href='https://vitejs.dev/'>
+                                    <img className='skills-icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vitejs/vitejs-original.svg" alt='Vite' />
+                                    <p className='skills-text'>
+                                        Vite
+                                    </p>
+                                </a>
+                            </div>
+                        </Col>
+                        <Col>
+                            <div className='skill-container'>
+                                <a href='https://webpack.js.org/'>
+                                    <img className='skills-icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/webpack/webpack-plain.svg" alt='Webpack' />
+                                    <p className='skills-text'>
+                                        Webpack
+                                    </p>
+                                </a>
+                                
+                            </div>
+                        </Col>
+                        <Col>
+                            <div className='skill-container'>
+                                <a href='https://expressjs.com/'>
+                                    <img className='skills-icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg" alt='Express' />
+                                    <p className='skills-text'>
+                                        Express
+                                    </p>
+                                </a>
+                                
                             </div>
                         </Col>
                         
                         <Col>
                             <div className='skill-container'>
-                                <img className='skills-icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg" alt='MongoDB' />
-                                <p className='skills-text'>
-                                    MongoDB
-                                </p>
+                                <a href='https://nodejs.org/'>
+                                    <img className='skills-icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" alt='node.js' />
+                                    <p className='skills-text'>
+                                        Node.js
+                                    </p>
+                                </a>
+                            </div>
+                        </Col>
+                        
+                        <Col>
+                            <div className='skill-container'>
+                                <a href='https://www.mongodb.com/'>
+                                    <img className='skills-icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg" alt='MongoDB' />
+                                    <p className='skills-text'>
+                                        MongoDB
+                                    </p>
+                                </a>
                             </div>
                         </Col>
                         <Col>
                             <div className='skill-container'>
-                                <img className='skills-icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postgresql/postgresql-original.svg" alt='PostgreSQL' />
-                                <p className='skills-text'>
-                                    PostgreSQL
-                                </p>
+                                <a href='https://www.postgresql.org/'>
+                                    <img className='skills-icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postgresql/postgresql-original.svg" alt='PostgreSQL' />
+                                    <p className='skills-text'>
+                                        PostgreSQL
+                                    </p>
+                                </a>
+                                
                             </div>
                         </Col>
                         <Col>
                             <div className='skill-container'>
-                                <img className='skills-icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/jest/jest-plain.svg" alt='Jest' />
-                                <p className='skills-text'>
-                                    Jest
-                                </p>
+                                <a href='https://jestjs.io/'>
+                                    <img className='skills-icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/jest/jest-plain.svg" alt='Jest' />
+                                    <p className='skills-text'>
+                                        Jest
+                                    </p>
+                                </a>
                             </div>
                         </Col>
                         <Col>
                             <div className='skill-container'>
-                                <img className='skills-icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mocha/mocha-original.svg" alt='Mocha' />
-                                <p className='skills-text'>
-                                    Mocha
-                                </p>
+                                <a href='https://mochajs.org/'>
+                                    <img className='skills-icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mocha/mocha-original.svg" alt='Mocha' />
+                                    <p className='skills-text'>
+                                        Mocha
+                                    </p>
+                                </a>
                             </div>
                         </Col>
                         <Col>
                             <div className='skill-container'>
-                                <img className='skills-icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg" alt='TailwindCSS' />
-                                <p className='skills-text'>
-                                    TailwindCSS
-                                </p>
+                                <a href='https://tailwindcss.com/'>
+                                    <img className='skills-icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg" alt='TailwindCSS' />
+                                    <p className='skills-text'>
+                                        TailwindCSS
+                                    </p>
+                                </a>
+                                
                             </div>
                         </Col>
                         <Col>
                             <div className='skill-container'>
-                                <img className='skills-icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/googlecloud/googlecloud-original.svg" />
-                                <p className='skills-text'>
-                                    Google Cloud
-                                </p>
+                                <a href='https://cloud.google.com/'>
+                                    <img className='skills-icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/googlecloud/googlecloud-original.svg" />
+                                    <p className='skills-text'>
+                                        Google Cloud
+                                    </p>
+                                </a>
                             </div>
                         </Col>
                         <Col>
                             <div className='skill-container'>
-                                <img className='skills-icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/docker/docker-original.svg" />
-                                <p className='skills-text'>
-                                    Docker
-                                </p>
+                                <a href='https://www.docker.com/'>
+                                    <img className='skills-icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/docker/docker-original.svg" />
+                                    <p className='skills-text'>
+                                        Docker
+                                    </p>
+                                </a>
                             </div>
                         </Col>
                     </Row>
