1031b377201a697946611fe8a83753b2c59de914
rosecodez <rosebeats09@gmail.com>
2025-03-25 23:09:42 -0400

    Make header fixed

--- src/HeaderLinks.jsx
+++ src/HeaderLinks.jsx
@@ -4,28 +4,26 @@ import { Link as ScrollLink } from 'react-scroll';
 
 export default function HeaderLinks() {
     return (
-      <>
-        <Row id='header-container'>
-          <Row id='header-links-container'>
-            <Col xl={2} lg={4} sm={6} id='header-link'>
-              <ScrollLink to='projects' smooth={true} duration={500}>
-                Projects
-              </ScrollLink>
-            </Col>
-            <Col xl={2} lg={4} sm={6} id='header-link'>
-              <ScrollLink to='about' smooth={true} duration={500}>
-                About
-              </ScrollLink>
-            </Col>
-            <Col xl={2} lg={4} sm={6} id='header-link'>
-              <ScrollLink to='skills' smooth={true} duration={500}>
-                Skills
-              </ScrollLink>
-            </Col>
-            
-          </Row>
+      <Row id='header-container'>
+        <Row id='header-links-container'>
+          <Col xl={2} lg={4} sm={6} id='header-link'>
+            <ScrollLink to='projects' smooth={true} duration={500}>
+              Projects
+            </ScrollLink>
+          </Col>
+          <Col xl={2} lg={4} sm={6} id='header-link'>
+            <ScrollLink to='about' smooth={true} duration={500}>
+              About
+            </ScrollLink>
+          </Col>
+          <Col xl={2} lg={4} sm={6} id='header-link'>
+            <ScrollLink to='skills' smooth={true} duration={500}>
+              Skills
+            </ScrollLink>
+          </Col>
+          
         </Row>
-      </>
+      </Row>
     );
   }
   
