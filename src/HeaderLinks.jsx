bea0e8eefff958eef9807ca527292188ee63729a
rosecodez <rosebeats09@gmail.com>
2025-04-01 22:58:42 -0400

    Convert dropdwon into hamburger menu

--- src/HeaderLinks.jsx
+++ src/HeaderLinks.jsx
@@ -1,27 +1,28 @@
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import { Link as ScrollLink } from 'react-scroll';
 import Signature from "../src/images/signature.png";
 import DownloadIcon from "./images/icons8-download-24.png";
 
 export default function HeaderLinks() {
+  function displayMenu() {
+    const hamburgerMenu = document.getElementById("hamburger-menu");
+    if (hamburgerMenu.style.display === "flex") {
+      hamburgerMenu.style.display = "none";
+    } else {
+      hamburgerMenu.style.display = "flex";
+    }
+  }
+  
   return (
     <header id="header-container">
-        <ScrollLink to='home'  smooth={true} duration={500} href="#">
+      <div id='menu'>
+        <ScrollLink to='home' id='signature-container'  smooth={true} duration={500} href="#">
           <img id="signature-image" src={Signature} alt="Signature" />
         </ScrollLink>
+
         <div id="header-links-container">
-        
-          {/* dropdown button visible below 1054px */}
-          <div className="dropdown">
-            <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-            </button>
-            <ul className="dropdown-menu">
-              <li><ScrollLink to='projects' smooth={true} duration={500} className="dropdown-item" href="#">Projects</ScrollLink></li>
-              <li><ScrollLink to='about' smooth={true} duration={500} className="dropdown-item" href="#">About</ScrollLink></li>
-              <li><ScrollLink to='skills' smooth={true} duration={500} className="dropdown-item" href="#">Skills</ScrollLink></li>
-            </ul>
-          </div>
+          <button type="button" id='menu-btn' onClick={displayMenu}>
+            <img width="40" height="40" src="https://img.icons8.com/ios/50/menu--v7.png" alt="menu--v7"/>
+          </button>
 
           <ScrollLink to='projects' id='header-link' smooth={true} duration={500}>Projects</ScrollLink>
           <ScrollLink to='about' id='header-link' smooth={true} duration={500}>About</ScrollLink>
@@ -29,10 +30,20 @@ export default function HeaderLinks() {
           
           <a href='path_to_file' download={Signature} id='download-resume'>
             <p>Resume</p>
-            <img id="download-icon" src={DownloadIcon} alt="DownloadIcon" />
+            <img className="download-icon" src={DownloadIcon} alt="DownloadIcon" />
           </a>
         </div>
+
+      </div>
+      
+      {/* dropdown button visible below 1054px */}
+      <ul id='hamburger-menu'>
+        <li><ScrollLink to='projects' id='hamburger-link'  smooth={true} duration={500}>Projects</ScrollLink></li>
+        <li><ScrollLink to='about' id='hamburger-link' smooth={true} duration={500} >About</ScrollLink></li>
+        <li><ScrollLink to='skills' id='hamburger-link' smooth={true} duration={500} >Skills</ScrollLink></li>
+      </ul>
     </header>
+    
   );
 }
   
