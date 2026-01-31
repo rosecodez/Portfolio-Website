a7be3094a5914cfbee1bc9bf4057d221c26b2ca1
Roxana Dandu <rosebeats09@gmail.com>
2026-01-31 14:16:14 -0500

    Include theme toggle button into footer

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,10 +1,11 @@
-
+import ThemeToggleButton from "./ThemeToggleButton"
 export default function Footer() {
     return (
         <div id="footer-container">
             <div id='footer-text'>
                 Thank you for viewing my portfolio site today!
             </div>
+            <ThemeToggleButton />
         </div>
     )
 }
